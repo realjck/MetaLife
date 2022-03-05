689298daff49a43c4fe21f9b594cd254ecf5f206
realjck <jckleinbourg@gmail.com>
2022-03-05 19:43:06 +0100

    Add Pure Poly Forest + fix characters rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector3 spawnPosition = new Vector3(0, 0, 0);
+    private Vector3 spawnPosition = new Vector3(0, 0.5f, 0);
     [SerializeField] private float speed = 4.5f;
     [SerializeField] private float rotationSpeed = 80f;
     private float jumpForce = 4.5f;
