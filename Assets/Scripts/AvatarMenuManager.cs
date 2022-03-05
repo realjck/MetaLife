03385eb9b2909f9849204ed7db75c7864d37b7bd
realjck <jckleinbourg@gmail.com>
2022-03-05 21:23:05 +0100

    Change menu scene name

--- Assets/Scripts/AvatarMenuManager.cs
+++ Assets/Scripts/AvatarMenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
+public class AvatarMenuManager : MonoBehaviour
 {
     [SerializeField] private Vector3 characterPosition;
     private GameObject character;
